805553580ad42853d237729014f4380572c52937
Elya_1011 <elviraalimova06@gmail.com>
2024-12-20 01:11:09 +0300

    Polymorphism and Magic Methods

--- oop.py
+++ oop.py
@@ -7,6 +7,13 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за домашние задания: {sum(self.grades.values()) / len(self.grades)}\n'
+                f'Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n'
+                f'Завершенные курсы: {','.join(self.finished_courses)}')
+
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
@@ -15,6 +22,16 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def best_student(self, student_1, student_2):
+        if isinstance(student_1, Student) and isinstance(student_2, Student):
+            if sum(student_1.grades.values()) / len(student_1.grades) > sum(student_2.grades.values()) / len(student_2.grades):
+                return f'{student_1.name } {student_1.surname}'
+            elif sum(student_1.grades.values()) / len(student_1.grades) < sum(student_2.grades.values()) / len(student_2.grades):
+                return f'{student_2.name } {student_2.surname}'
+            else:
+                return f'Средние оценки за лекции равны'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -28,6 +45,21 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self):
+        return (f"имя: {self.name}\n"
+                f"фамилия: {self.surname}\n"
+                f"Средняя оценка за лекции: {sum(self.grades.values()) / len(self.grades)}")
+
+    def best_lecturer(self, lecturer_1, lecturer_2):
+        if isinstance(lecturer_1, Lecturer) and isinstance(lecturer_2, Lecturer):
+            if sum(lecturer_1.grades.values()) / len(lecturer_1.grades) > sum(lecturer_2.grades.values()) / len(lecturer_2.grades):
+                return f'{lecturer_1.name} {lecturer_1.surname}'
+            elif sum(lecturer_1.grades.values()) / len(lecturer_1.grades) < sum(lecturer_2.grades.values()) / len(lecturer_2.grades):
+                return f'{lecturer_2.name} {lecturer_2.surname}'
+            else:
+                return f'Средние оценки за лекции равны'
+
+
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
@@ -40,3 +72,8 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return (f"имя: {self.name}\n"
+                f"фамилия: {self.surname}")
+
